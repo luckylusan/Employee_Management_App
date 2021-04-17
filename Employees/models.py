5d03de2ed9da90cbfc25223a5d7a00a09ca72c93
littlekumar <57680556+littlekumar@users.noreply.github.com>
2021-04-17 15:55:36 +0530

    sample project

--- Employees/models.py
+++ Employees/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Employee(models.Model):
-    employee_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100, blank=False)
-    surname = models.CharField(max_length=100, blank=False)
-    email = models.EmailField(max_length=100,unique=True, blank=False)
+    sno = models.AutoField(primary_key=True)
+    employee_id = models.IntegerField()
+    emp_name = models.CharField(max_length=100, blank=False)
+    emp_designation = models.CharField(max_length=100, blank=False)
+    emp_doj = models.EmailField(max_length=100,unique=True, blank=False)
     owner = models.ForeignKey(
         User, related_name="employees", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'employees'
